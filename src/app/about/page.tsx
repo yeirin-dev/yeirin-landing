4dd794386fc2a63e241d1470ecf31a32b00cb978
hxont <queenly9@icloud.com>
2026-05-08 17:02:05 +0900

    ⚙️fix: 코드리뷰 반영 - board 파일명 통일 및 PopupAd.tsx Prettier 포매팅

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import Image from "next/image";
 import Link from "next/link";
 import {
   boardMembers,
-  fieldExpertMembers, getMemberById,
+  fieldExpertMembers,
   getMembersByIds,
   type Member,
 } from "@/data/members";
